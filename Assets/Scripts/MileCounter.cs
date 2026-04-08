f14e103c6b0398a8f3542e5b62a5bde29bd03db4
= <129625009+Tyson-Shannon@users.noreply.github.com>
2026-04-07 19:16:59 -0500

    MileCounter basics
    basic mile counter

---
+++ Assets/Scripts/MileCounter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MileCounter : MonoBehaviour
+{
+    private float milesTraveled;
+    private float carSpeed;
+
+    [SerializeField] private CarController car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        milesTraveled = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        carSpeed = car.GetSpeed();
+        //add miles
+        milesTraveled = milesTraveled + ((carSpeed) / 1000);
+        Debug.Log("Miles: "+milesTraveled);
+    }
+}
