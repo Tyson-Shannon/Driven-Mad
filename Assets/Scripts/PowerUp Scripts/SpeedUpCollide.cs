f848965b5ec1cc2b684a12009aa669d18a07f62a
= <129625009+Tyson-Shannon@users.noreply.github.com>
2026-04-09 19:15:19 -0500

    added powerup movement and object catcher
    object catcher should theoretically destroy objects once passed car

--- Assets/Scripts/PowerUp Scripts/SpeedUpCollide.cs
+++ Assets/Scripts/PowerUp Scripts/SpeedUpCollide.cs
@@ -3,6 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 //attach to speedup prefab
 //determines if player collides and adds boost before releasing object to pool
@@ -13,6 +14,9 @@ public class SpeedUpCollide : MonoBehaviour
     [SerializeField] private float boostAmount = 1f;
     [SerializeField] private float boostDuration = 5f;
 
+    [SerializeField] private CarController car;
+    private float carSpeed;
+
     private bool isCollected = false;//makes sure powerup isn't triggered more than once per life
 
     private void OnEnable()
@@ -20,18 +24,33 @@ public class SpeedUpCollide : MonoBehaviour
         isCollected = false;
     }
 
+    private void Update()
+    {
+        //move powerup to look like car drives towards it
+        carSpeed = car.GetSpeed();
+        transform.Translate(new Vector3(0, 0, -(carSpeed * Time.time)));
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        //if car collides with powerup
         if (!isCollected && other.CompareTag("Player"))
         {
-            isCollected = true; // Mark as used immediately
+            isCollected = true; //mark as used immediately
 
-            CarController car = other.GetComponent<CarController>();
             if (car != null)
             {
                 car.ApplySpeedUp(boostAmount, boostDuration);
             }
 
+            if (factory != null)
+            {
+                factory.ReleasePowerUp(gameObject);
+            }
+        }
+        //if powerup passes car it will eventually hit catcher and be released
+        if (other.CompareTag("ObjectCatcher"))
+        {
             if (factory != null)
             {
                 factory.ReleasePowerUp(gameObject);
