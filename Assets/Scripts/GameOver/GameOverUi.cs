7b56fa535587042adae45e153753ed1677df0bcc
vZeety <thierno.barry@mnsu.edu>
2026-05-05 13:46:18 -0500

    GameOver UI polished - score, distance, best score displaying correctly

---
+++ Assets/Scripts/GameOver/GameOverUi.cs
@@ -0,0 +1,53 @@
+//Thierno Barry 05/05/2026
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private TextMeshProUGUI finalScoreText;
+    [SerializeField] private TextMeshProUGUI bestScoreText;
+    [SerializeField] private TextMeshProUGUI finalDistanceText;
+
+    void OnEnable()
+    {
+        CarHealthManager.OnCarDestroyed += ShowGameOver;
+    }
+
+    void OnDisable()
+    {
+        CarHealthManager.OnCarDestroyed -= ShowGameOver;
+    }
+
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    void ShowGameOver(CarHealthManager.DamageSource source)
+    {
+        if (ScoreManager.Instance != null)
+        {
+            ScoreManager.Instance.SaveHighScore();
+            finalScoreText.text = "Score: " + ScoreManager.Instance.GetCurrentScore();
+            bestScoreText.text = "Best: " + ScoreManager.Instance.GetBestScore();
+            finalDistanceText.text = "Distance: " + ScoreManager.Instance.GetCurrentDistance().ToString("F1");
+        }
+        gameOverPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        if (ScoreManager.Instance != null)
+            ScoreManager.Instance.ResetRun();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMainMenu()
+    {
+        if (ScoreManager.Instance != null)
+            ScoreManager.Instance.ResetRun();
+        SceneManager.LoadScene("LevelSelector");
+    }
+}
